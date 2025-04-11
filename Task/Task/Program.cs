6e65a5a5473314f6cc5b683ee61924afcfcc188b
TuranSalimli09 <turanselimli09@gmail.com>
2025-04-11 11:36:56 +0400

    Reflection task

---
+++ Task/Task/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using Task;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Type type = typeof(User);
+
+        Assembly asm = Assembly.GetExecutingAssembly();
+        Type userType = asm.GetTypes().FirstOrDefault(t => t.Name == "User");
+
+        if (userType == null)
+        {
+            Console.WriteLine("User tipi tapılmadı.");
+            return;
+        }
+
+        object userInstance = Activator.CreateInstance(userType);
+
+        FieldInfo idField = userType.GetField("id", BindingFlags.NonPublic | BindingFlags.Instance);
+        FieldInfo nameField = userType.GetField("name", BindingFlags.NonPublic | BindingFlags.Instance);
+        FieldInfo surnameField = userType.GetField("surname", BindingFlags.NonPublic | BindingFlags.Instance);
+        FieldInfo ageField = userType.GetField("age", BindingFlags.NonPublic | BindingFlags.Static);
+
+        idField.SetValue(userInstance, 1);
+        nameField.SetValue(userInstance, "Turan");
+        surnameField.SetValue(userInstance, "Developer");
+        ageField.SetValue(null, 20);
+
+        Console.WriteLine($"id: {idField.GetValue(userInstance)}");
+        Console.WriteLine($"name: {nameField.GetValue(userInstance)}");
+        Console.WriteLine($"surname: {surnameField.GetValue(userInstance)}");
+        Console.WriteLine($"age: {ageField.GetValue(null)}");
+
+        MethodInfo getFullNameMethod = userType.GetMethod("GetFullName", BindingFlags.Public | BindingFlags.Instance);
+        getFullNameMethod.Invoke(userInstance, null);
+
+        MethodInfo changeAgeMethod = userType.GetMethod("ChangeAge", BindingFlags.Public | BindingFlags.Static);
+        changeAgeMethod.Invoke(null, new object[] { 25 });
+
+        MethodInfo showAgeMethod = userType.GetMethod("ShowAge", BindingFlags.Public | BindingFlags.Static);
+        
+    }
+}
+
+
---
+++ Task/Task/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class User
+    {
+        private int id;
+        private string name;
+        private string surname;
+        private static int age;
+
+        public void GetFullName()
+        {
+            Console.WriteLine($"{name} {surname}");
+        }
+
+        public static void ChangeAge(int newAge)
+        {
+            age = newAge;
+        }
+    }
+
+
+}
